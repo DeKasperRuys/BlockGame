8e4e743b14f0403a3aa0b652346de45563ff202f
Kasper <kasper_ruys@hotmail.com>
2022-08-12 19:16:45 +0200

    SET amount of levels (buttons)

--- BlockGame/Assets/RandomColourButtons.cs
+++ BlockGame/Assets/RandomColourButtons.cs
@@ -7,17 +7,73 @@ using UnityEngine.UI;
 
 public class RandomColourButtons : MonoBehaviour
 {
-
+    int count = 0;
     int randomColorNumber;
-    int count =0;
+    [SerializeField] private int buttonAmount = 2;
+    [SerializeField] public GameObject buttonPrefab;
+    [SerializeField] public Transform buttonContainer;
+
     void Start()
     {
+        
+        for (int i = 1; i < buttonAmount+1; i++)
+        {
+            int tempint = i - 1;
+            randomColorNumber = Random.Range(0, 7);
+            GameObject newButton = Instantiate(buttonPrefab) as GameObject;
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString();
+            newButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
 
+                PlayerPrefs.SetInt("level", tempint);
+                Debug.Log("level"+i);
+                SceneManager.LoadScene("game");
+
+            });
+            newButton.transform.SetParent(buttonContainer);
+
+
+            if (randomColorNumber == 0)
+            {
+                newButton.GetComponent<Image>().color = StaticData.Red;
+            }
+            if (randomColorNumber == 1)
+            {
+                newButton.GetComponent<Image>().color = StaticData.Green;
+            }
+            if (randomColorNumber == 2)
+            {
+                newButton.GetComponent<Image>().color = StaticData.Blue;
+            }
+            if (randomColorNumber == 3)
+            {
+                newButton.GetComponent<Image>().color = StaticData.Orange;
+            }
+            if (randomColorNumber == 4)
+            {
+                newButton.GetComponent<Image>().color = StaticData.Purple;
+            }
+            if (randomColorNumber == 5)
+            {
+                newButton.GetComponent<Image>().color = StaticData.Yellow;
+            }
+            if (randomColorNumber == 6)
+            {
+                newButton.GetComponent<Image>().color = StaticData.Cyan;
+            }
+        }
+
+
+
+        /*
         foreach (Transform child in transform)
         {
             count++;
             randomColorNumber = Random.Range(0, 6);
+
+            //Set button text to the correct level
             child.GetComponentInChildren<TextMeshProUGUI>().text = count.ToString();
+
             int tempint = count;
 
             child.GetComponent<Button>().onClick.AddListener(() =>
@@ -59,10 +115,11 @@ public class RandomColourButtons : MonoBehaviour
             }
             
             
-        }
+        }*/
 
 
     }
 
-   
+
+
 }
